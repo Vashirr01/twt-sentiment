9b1fbe8f19b08c3c0950833a4d587980de90b85b
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-18 19:51:57 +0530

    completed class def with get_tweets fn to fetch count no of tweets with the api and parse

--- analysis.py
+++ analysis.py
@@ -4,10 +4,9 @@ import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
 
-class TwitterClient(object):
 
+class TwitterClient:
     def __init__(self):
-
         consumer_key = os.environ.get('API_KEY')
         consumer_secret = os.environ.get('API_SECRET')
         access_token = os.environ.get('ACC_TOKEN')
@@ -18,27 +17,48 @@ class TwitterClient(object):
             self.auth.set_access_token(access_token, access_token_secret)
             self.api = tweepy.API(self.auth)
         except:
-            print(&quot;Error: Authentication Failed&quot;)
+            print("Error: Authentication Failed")
 
     def clean_tweet(self, tweet):
-
-        return ' '.join(re.sub(&quot;(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)&quot;, &quot; &quot;,tweet).split())
+        """Remove special characters and links from tweet."""
+        return ' '.join(
+            re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)",
+                  " ",
+                  tweet).split())
 
     def get_tweet_sentiment(self, tweet):
-
+        """Determine sentiment of tweet using TextBlob."""
         analysis = TextBlob(self.clean_tweet(tweet))
-
-        if analysis.sentiment.polarity &gt; 0:
+        
+        if analysis.sentiment.polarity > 0:
             return 'positive'
         elif analysis.sentiment.polarity == 0:
             return 'neutral'
         else:
             return 'negative'
 
-    def get_tweets(self, query, count = 5):
-
+    def get_tweets(self, query, count=5):
+        """Fetch tweets and return list with text and sentiment."""
         tweets = []
 
-        tr
+        try:
+            fetched_tweets = self.api.search(q=query, count=count)
+            
+            for tweet in fetched_tweets:
+                parsed_tweet = {
+                    'text': tweet.text,
+                    'sentiment': self.get_tweet_sentiment(tweet.text)
+                }
+                
+                if tweet.retweet_count > 0:
+                    if parsed_tweet not in tweets:
+                        tweets.append(parsed_tweet)
+                else:
+                    tweets.append(parsed_tweet)
+                    
+            return tweets
+
+        except tweepy.TweepError as e:
+            print("Error: " + str(e))
 
 
