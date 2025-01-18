4f75c46f7e6e05faac48a7a1acbd69079b5fc0c2
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-18 18:56:55 +0530

    added gitignore and basic class def for twt client

---
+++ analysis.py
@@ -0,0 +1,12 @@
+import re
+import tweepy
+from tweepy import OAuthHandler
+from textblob import TextBlob
+
+class TwitterClient(object):
+
+    def __init__(self):
+        consumer_key = 
+        consumer_secret = 
+        access_token = 
+        access_token_secret = 
