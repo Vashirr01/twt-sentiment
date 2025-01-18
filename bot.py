ef8f46ba991b827e1cba65be88f0bea931d998c9
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-19 01:48:12 +0530

    completed the bot which retweets, likes and follows anything provided by the query hashtag

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
 import tweepy
+import os
 from time import sleep
-from config import QUERY, FOLLOW, LIKE, SLEEP_TIME
 from dotenv import load_dotenv
+
 load_dotenv()
 
+#configuration
+QUERY = '# saifalikhan'
+LIKE = True
+FOLLOW = True
+SLEEP_TIME = 150
 
 bearer_token = os.environ.get('BEARER_TOKEN')
 client = tweepy.Client(
@@ -15,6 +21,26 @@ print("bot settings")
 print("like tweets:", LIKE)
 print("follow users:", FOLLOW)
 
-for tweet in tweepy.Cursor(api.search, q=QUERY).items():
+for tweet in tweepy.Cursor(client.search, q=QUERY).items():
+
     try:
-        print('\nTweet by: ')
+        print('\nTweet by: @' + tweet.user.screen_name)
+        tweet.retweet()
+        print("Retweeted the tweet")
+
+        if LIKE:
+            tweet.favorite()
+            print("Favorited the tweet")
+
+        if FOLLOW:
+            if not tweet.user.following:
+                tweet.user.follow()
+                print("Followed the user")
+
+        sleep(SLEEP_TIME)
+
+    except tweepy.TweepError as e:
+        print(e.reason)
+
+    except StopIteration:
+        break
