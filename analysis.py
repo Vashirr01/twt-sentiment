b3dcd0633ac07bdaf8271d372b19e687e0eb5b2b
vashirr <130561897+Vashirr01@users.noreply.github.com>
2025-01-18 19:27:32 +0530

    clean tweet and get sentiment functions added to the existing def

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import re
+import os
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
@@ -6,7 +7,38 @@ from textblob import TextBlob
 class TwitterClient(object):
 
     def __init__(self):
-        consumer_key = 
-        consumer_secret = 
-        access_token = 
-        access_token_secret = 
+
+        consumer_key = os.environ.get('API_KEY')
+        consumer_secret = os.environ.get('API_SECRET')
+        access_token = os.environ.get('ACC_TOKEN')
+        access_token_secret = os.environ.get('ACC_SECRET')
+
+        try:
+            self.auth = OAuthHandler(consumer_key, consumer_secret)
+            self.auth.set_access_token(access_token, access_token_secret)
+            self.api = tweepy.API(self.auth)
+        except:
+            print(&quot;Error: Authentication Failed&quot;)
+
+    def clean_tweet(self, tweet):
+
+        return ' '.join(re.sub(&quot;(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)&quot;, &quot; &quot;,tweet).split())
+
+    def get_tweet_sentiment(self, tweet):
+
+        analysis = TextBlob(self.clean_tweet(tweet))
+
+        if analysis.sentiment.polarity &gt; 0:
+            return 'positive'
+        elif analysis.sentiment.polarity == 0:
+            return 'neutral'
+        else:
+            return 'negative'
+
+    def get_tweets(self, query, count = 5):
+
+        tweets = []
+
+        tr
+
+
